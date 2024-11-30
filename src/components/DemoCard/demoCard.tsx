0f90db4a1661536de7c7bda3599c0cde52746958
Sudipta Ghosh <uadayghosh@gmail.com>
2024-11-30 23:48:55 +0600

    configuration updated

--- src/components/DemoCard/demoCard.tsx
+++ src/components/DemoCard/demoCard.tsx
@@ -1,4 +1,7 @@
 import { BellRing, Check } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
@@ -6,10 +9,8 @@ import {
 	CardFooter,
 	CardHeader,
 	CardTitle,
-} from "../ui/card";
-import { cn } from "../../lib/utils";
-import { Switch } from "../ui/switch";
-import { Button } from "../ui/button";
+} from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
 
 const notifications = [
 	{
